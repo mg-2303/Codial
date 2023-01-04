e3ef6cb010bbda7e5a120bf5659d4adbdac839bb
Manav Gohel <2303manavgohel@gmail.com>
2023-01-04 10:44:32 +0530

    Add sign-in and Sign-up dommy pages

--- controller/users_controllers.js
+++ controller/users_controllers.js
@@ -1,3 +1,17 @@
 module.exports.profile = function (req, res) {
     res.end('<h1>User Profile</h1>')
 }
+
+// render the sign up page
+module.exports.signIn = function (req, res) {
+    return res.render('user_sign_in', {
+        title: 'Codial|Sign In'
+    });
+}
+
+// render the sign in page
+module.exports.signUp = function (req, res) {
+    return res.render('user_sign_up', {
+        title: 'Codial | Sign Up'
+    });
+}
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,7 @@ const usersController = require('../controller/users_controllers');
 const router = express.Router();
 
 router.get('/profile', usersController.profile);
+router.get('/sign-in', usersController.signIn);
+router.get('/sign-up', usersController.signUp);
 
 module.exports = router;
