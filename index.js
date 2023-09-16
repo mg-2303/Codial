cb90c696f9135067cd23c6e98eff2030f479080c
Manav Gohel <2303manavgohel@gmail.com>
2023-09-16 08:47:24 +0530

    Second Commit: Setting up User_contoller and Express Router

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const port = 8000;
-
-
-
 const app = express();
+
+
+//Use Express Router
+app.use('/', require('./routes'));
 
 app.listen(port, function (err) {
     if (err) {
